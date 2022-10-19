585375773057d5d410dd8cff35d16f36ea0e12cf
tatarynrm <tatarynrm@gmail.com>
2022-10-19 15:38:10 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,14 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import { useEffect } from "react";
 import { useState } from "react";
+import { sucessfulLookup } from "./services/userGeolocation";
 
 const App = () => {
   const [userLocation, setUserLocation] = useState([]);
 
-  const sucessfulLookup = (position) => {
-    const { latitude, longitude } = position.coords;
-    fetch(
-      `https://api.bigdatacloud.net/data/reverse-geocode-client?${latitude}&${longitude}&localityLanguage=en`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const preData = [];
-        preData.push(data);
-
-        setUserLocation(preData);
-      });
-  };
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(sucessfulLookup, console.log);
   }, []);
-
   return (
     <>
       <Header />
---
+++ src/services/userGeolocation.js
@@ -0,0 +1,13 @@
+export const sucessfulLookup = (position, setUserLocation) => {
+  const { latitude, longitude } = position.coords;
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?${latitude}&${longitude}&localityLanguage=en`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const preData = [];
+      preData.push(data);
+
+      setUserLocation(preData);
+    });
+};
